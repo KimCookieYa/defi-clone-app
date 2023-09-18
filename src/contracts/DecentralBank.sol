b81fb9c40472a00fa92125174d997c480808d26d
KimCookieYa <min49590@gmail.com>
2023-09-19 02:56:57 +0900

    Feat: staking 구현

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -9,9 +9,34 @@ contract DecentralBank {
     RWD public rwd;
     Tether public tether;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
         owner = msg.sender;
+    }
+
+    // staking function
+    function depositTokens(uint _amount) public {
+        // Require staking amount to be greater than 0
+        require(_amount > 0, 'Amount must be greater than 0');
+        
+        // Transfer tether tokens to this contract address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        // Update Staking Balance
+        stakingBalance[msg.sender] += _amount;
+
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        hasStaked[msg.sender] = true;
+        isStaked[msg.sender] = true;
     }
 }
